4a010908caa68bb3954c94b9895548f9f8eb1017
jcpriyanto <76898754+jcpriyanto@users.noreply.github.com>
2022-05-25 02:21:11 -0700

    Added id for settings button Icon and fixed JS bug

--- js/Misc/Settings.js
+++ js/Misc/Settings.js
@@ -47,9 +47,10 @@ const root = document.querySelector(':root');
 /**
  * handle the settings popup display
  */
+
 document.addEventListener('click', (event) => {
   if (settingsPopup.style.display === 'block') {
-    if (!settingsPopup.contains(event.target) && event.target.id !== 'settingsButton') {
+    if (!settingsPopup.contains(event.target) && (event.target.id !== 'settingsButton' && event.target.id !== 'settingsButtonIcon')) {
       settingsPopup.style.display = 'none';
     }
   }
@@ -59,7 +60,7 @@ settingsButton.addEventListener('click', () => {
   if (settingsPopup.style.display === 'block') {
     settingsPopup.style.display = 'none';
   } else {
-    settingsPopup.style.display = 'block';
+    settingsPopup.style.display = 'block';  
   }
 });
 
